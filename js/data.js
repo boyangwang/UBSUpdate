90d1c353035bd9a4d1024aeca0639a9b18ffc944
boyang <wangboyangjinghua@gmail.com>
2014-10-29 21:44:52 +0800

    added data.js -from git-all script

---
+++ js/data.js
@@ -0,0 +1,11 @@
+{
+	"linkedin": [
+		{"author": "bla", "title":"foobar"}, // pulse 1
+		{"author": "bla", "title":"foobar"}, // pulse 2
+	],
+	"company overview": [
+		{"name": "sth"},
+	],
+	"tweets": 
+
+}
